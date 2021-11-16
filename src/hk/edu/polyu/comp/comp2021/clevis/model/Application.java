ae113fa8df23da24bb2f14eb43b1c2b20fe6b74a
Hans <sweatermate@gmail.com>
2021-11-16 17:25:23 +0800

    hello

--- src/hk/edu/polyu/comp/comp2021/clevis/model/Application.java
+++ src/hk/edu/polyu/comp/comp2021/clevis/model/Application.java
@@ -3,7 +3,8 @@ package hk.edu.polyu.comp.comp2021.clevis.model;
 
 public class Application {
 
-    // Launch the Clevis Software
+    // Launch the Clevis Softwareabc
+
     public static void main(String[] args){
         Clevis clevis = new Clevis();
     }
