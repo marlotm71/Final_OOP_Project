67d88c734e75aaa688520cccb7ed0a622628c8d9
marlotm71 <92908405+marlotm71@users.noreply.github.com>
2021-11-16 17:21:45 +0800

    Version Tuesday 17:11

--- src/hk/edu/polyu/comp/comp2021/clevis/model/Application.java
+++ src/hk/edu/polyu/comp/comp2021/clevis/model/Application.java
@@ -3,7 +3,7 @@ package hk.edu.polyu.comp.comp2021.clevis.model;
 
 public class Application {
 
-
+    // Launch the Clevis Software
     public static void main(String[] args){
         Clevis clevis = new Clevis();
     }
