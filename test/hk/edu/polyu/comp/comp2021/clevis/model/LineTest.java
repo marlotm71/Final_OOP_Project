d762ea6840a5d9ad9e9456a714906e1a85f51bca
marlotm71 <92908405+marlotm71@users.noreply.github.com>
2021-11-17 18:13:15 +0800

    Version Wednesday 17:23 Magic number Line Test

--- test/hk/edu/polyu/comp/comp2021/clevis/model/LineTest.java
+++ test/hk/edu/polyu/comp/comp2021/clevis/model/LineTest.java
@@ -14,477 +14,136 @@ import static org.junit.jupiter.api.Assertions.*;
  * Class Line Test
  */
 class LineTest {
-    private double liny1;
-
-    {
-        liny1 = 2.0;
-    }
-
-    private double linx2;
-
-    {
-        linx2 = 8.0;
-    }
-
-    private double liny2;
-
-    {
-        liny2 = 7.0;
-    }
-
-    private double gautx1;
-
-    {
-        gautx1 = 2.0;
-    }
-
-    private double gauty1;
-
-    {
-        gauty1 = 7.0;
-    }
-
-    private double gautx2;
-
-    {
-        gautx2 = 5.0;
-    }
-
-    private double gauty2;
-
-    {
-        gauty2 = 10.0;
-    }
-
-    private double marionx1;
-
-    {
-        marionx1 = 8.0;
-    }
-
-    private double mariony1;
-
-    {
-        mariony1 = 2.0;
-    }
-
-    private double marionx2;
-
-    {
-        marionx2 = 13.0;
-    }
-
-    private double mariony2;
-
-    {
-        mariony2 = 2.0;
-    }
-
-    private double x1lin;
-
-    {
-        x1lin = 4;
-    }
-
-    private double gaut1X1;
-    {
-        gaut1X1 = 3.0;
-    }
-
-    private double gaut1Y1;
-    {
-        gaut1Y1 = 2.0;
-    }
-
-    private double gaut1X2;
-    {
-        gaut1X2 = 7.0;
-    }
-
-    private double gaut1Y2;
-    {
-        gaut1Y2 = 7.0;
-    }
-
-    private double marion1X1;
-    {
-        marion1X1 = 3.0;
-    }
-
-    private double marion1y1;
-    {
-        marion1y1 = 4.0;
-    }
-
-    private double marion1x2;
-    {
-        marion1x2 = 7.0;
-    }
-
-    private double marion1y2;
-    {
-        marion1y2 = 4.0;
-    }
-
-    private double romainX1;
-    {
-        romainX1 = 10.0;
-    }
-
-    private double romainY1;
-    {
-        romainY1 = 4.0;
-    }
-
-    private double romainX2;
-    {
-        romainX2 = 10.0;
-    }
-
-    private double romainY2;
-    {
-        romainY2 = 8.0;
-    }
-
-    private double qqX1;
-    {
-        qqX1 = 5.0;
-    }
-
-    private double qqY1;
-    {
-        qqY1 = 7.8;
-    }
-
-    private double qqX2;
-    {
-        qqX2 = 12.0;
-    }
-
-    private double qqY2;
-    {
-        qqY2 = 13.0;
-    }
-
-    private double vincentX1;
-    {
-        vincentX1 = 4.0;
-    }
-
-    private double vincentY1;
-    {
-        vincentY1 = 8.0;
-    }
-
-    private double vincentX2;
-    {
-        vincentX2 = 9.0;
-    }
-
-    private double vincentY2;
-    {
-        vincentY2 = 4.0;
-    }
-
-    private double jeanX1;
-    {
-        jeanX1 = 4.0;
-    }
-
-    private double jeanY1;
-    {
-        jeanY1 = 6.0;
-    }
-
-    private double jeanX2;
-    {
-        jeanX2 = 4.0;
-    }
-
-    private double jeanY2;
-    {
-        jeanY2 = 12.0;
-    }
-
-    private double Theresey2;
-    {
-        Theresey2 = 12.0;
-    }
-
-    private double flavieY1;
-    {
-        flavieY1 = 11.0;
-    }
-
-    private double flavieY2;
-    {
-        flavieY2 = 15.0;
-    }
-
-    private double JacqueY1;
-    {
-        JacqueY1 = 16.0;
-    }
-
-    private double JacqueY2;
-    {
-        JacqueY2 = 19.0;
-    }
-
-    private double ddY1;
-    {
-        ddY1 = 14.0;
-    }
-
-    private double ddY2;
-    {
-        ddY2 = 16.0;
-    }
-
-    private double circle3Y;
-    {
-        circle3Y = 17.0;
-    }
-
-    private double SaraAE;
-    {
-        SaraAE = 13.0;
-    }
-
-    private double SaraX1;
-    {
-        SaraX1 = 12.0;
-    }
-
-    private double SaraY1;
-    {
-        SaraY1 = 32.0;
-    }
-
-    private double SaraX2;
-    {
-        SaraX2 = 13.0;
-    }
-
-    private double SaraY2;
-    {
-        SaraY2 = 26.0;
-    }
-
-    private double HenryE;
-    {
-        HenryE = 34.0;
-    }
-
-    private double HenryX1;
-    {
-        HenryX1 = 33.0;
-    }
-
-    private double HenryY1;
-    {
-        HenryY1 = 12.0;
-    }
-
-    private double HenryX2;
-    {
-        HenryX2 = 54.0;
-    }
-
-    private double HenryY2;
-    {
-        HenryY2 = 34.0;
-    }
-
-    private double LemX1;
-    {
-        LemX1 = 32.0;
-    }
-
-    private double LemY1;
-    {
-        LemY1 = 32.0;
-    }
-
-    private double LemX2;
-    {
-        LemX2 = 32.0;
-    }
-
-    private double LemY2;
-    {
-        LemY2 = 32.0;
-    }
-
-    private double LemmyX1;
-    {
-        LemmyX1 = 15.0;
-    }
-    private double LemmyY1;
-    {
-        LemmyY1 = 20.0;
-    }
-
-    private double LemmyX2;
-    {
-        LemmyX2 = 25.0;
-    }
-
-    private double LemmyY2;
-    {
-        LemmyY2 = 30.0;
-    }
-
-    private double KarenX1;
-    {
-        KarenX1 = 15.0;
-    }
-
-    private double KarenY1;
-    {
-        KarenY1 = 20.0;
-    }
-
-    private double KarenX2;
-    {
-        KarenX2 = 35.0;
-    }
-
-    private double KarenY2;
-    {
-        KarenY2 = 40.0;
-    }
-
-    private double KerryAE;
-    {
-        KerryAE = 28.0;
-    }
-
-    private double KerryX1;
-    {
-        KerryX1 = 15.0;
-    }
-
-    private double KerryY1;
-    {
-        KerryY1 = 25.0;
-    }
-
-    private double KerryX2;
-    {
-        KerryX2 = 40.0;
-    }
-
-    private double KerryY2;
-    {
-        KerryY2 = 20.0;
-    }
-
-    private double linAEX2;
-    {
-        linAEX2 = 12.0;
-    }
-
-    private double linAEY2;
-    {
-        linAEY2 = 12.0;
-    }
-
-    private double boratX1;
-    {
-        boratX1 = 3.0;
-    }
-
-    private double boratY1;
-    {
-        boratY1 = 12.0;
-    }
-
-    private double boratX2;
-    {
-        boratX2 = 9.0;
-    }
-
-    private double boratY2;
-    {
-        boratY2 = 34.0;
-    }
-
-    private double KhanAE;
-    {
-        KhanAE = 12.0;
-    }
-
-    private double KhanX1;
-    {
-        KhanX1 = 12.0;
-    }
-
-    private double KhanY1;
-    {
-        KhanY1 = 5.0;
-    }
-
-    private double JacqueY2;
-    {
-        JacqueY2 = 19.0;
-    }
-
-    private double JacqueY2;
-    {
-        JacqueY2 = 19.0;
-    }
-
-    private double JacqueY2;
-    {
-        JacqueY2 = 19.0;
-    }
-
-    private double JacqueY2;
-    {
-        JacqueY2 = 19.0;
-    }
-
-    private double JacqueY2;
-    {
-        JacqueY2 = 19.0;
-    }
-
-    private double JacqueY2;
-    {
-        JacqueY2 = 19.0;
-    }
-
-
-    private Line lin = new Line("Kim", 2, x1lin,liny1,linx2,liny2);
-    private Line gaut= new Line("Gaut", 2, gautx1,gauty1,gautx2,gauty2);
-    private Line marion= new Line("Marion", 2, marionx1,mariony1,marionx2,mariony2);
-    private Line gaut1= new Line("Gaut1", 2, gaut1X1,gaut1Y1,gaut1X2,gaut1Y2);
-    private Line marion1= new Line("Marion1", 2, marion1X1,marion1y1,marion1x2,marion1y2);
-    private Line romain= new Line("Romain", 2, romainX1,romainY1,romainX2,romainY2);
-    private Line qq = new Line("Yanny",3, qqX1, qqY1, qqX2, qqY2);
-    private Line vincent = new Line("Vincent",3, vincentX1, vincentY1, vincentX2, vincentY2);
-    private Line jean = new Line("Jean",3, jeanX1, jeanY1, jeanX2, jeanY2);
-    private Line paul = new Line("Paul",3, 3, 10, 6, 10);
-    private Line therese = new Line("Therese",3, 2, 6, 2, Theresey2);
-    private Line pierre = new Line("Pierre",3, 4, 5, 9, 5);
-    private Line flavie = new Line("Flavie",3, 4, flavieY1, 4, flavieY2);
-    private Line claude = new Line("Claude",3, 1, 4, 5, 4);
-    private Line bernard = new Line("Bernard",3, 4, 8, 4, 10);
-    private Line Jacque = new Line("Jacque",3, 4, JacqueY1, 4, JacqueY2);
-    private Line dd = new Line("DD",3, 3, ddY1, 1, ddY2);
-    private Circle circle = new Circle("Circle", 2, 5, 4, 2);
-    private Circle circle2 = new Circle("Circle2", 2, 4, 9, 2);
-    private Circle circle3 = new Circle("Circle3", 2, 4, circle3Y, 3);
-    private Rectangle rectangle = new Rectangle("Rectangle", 4,4, 3, 3, 4);
-    private Rectangle rectangle2 = new Rectangle("Rectangle2",1, 2, 9 ,6,3);
-    private Square square = new Square("Square", 2,2, 3, 3);
+    private final int zorder =1;
+    private final double liny1 = 2.0;
+    private final double linx2 = 8.0;
+    private final double liny2 = 7.0;
+    private final double gautx1 = 2.0;
+    private final double gauty1 = 7.0;
+    private final double gautx2 = 5.0;
+    private final double gauty2 = 10.0;
+    private final double marionx1 = 8.0;
+    private final double mariony1 = 2.0;
+    private final double marionx2 = 13.0;
+    private final double mariony2 = 2.0;
+    private final double  x1lin = 4;
+    private final double gaut1X1 = 3.0;
+    private final double gaut1Y1 = 2.0;
+    private final double gaut1X2 = 7.0;
+    private final double  gaut1Y2 = 7.0;
+    private final double marion1X1 = 3.0;
+    private final double marion1y1 = 4.0;
+    private final double  marion1x2 = 7.0;
+    private final double marion1y2 = 4.0;
+    private final double romainX1 = 10.0;
+    private final double romainY1 = 4.0;
+    private final double romainX2 = 10.0;
+    private final double romainY2 = 8.0;
+    private final double vincentX1 = 4.0;
+    private final double vincentY1 = 8.0;
+    private final double vincentX2 = 9.0;
+    private final double vincentY2 = 4.0;
+    private final double jeanX1 = 4.0;
+    private final double jeanY1 = 6.0;
+    private final double jeanX2 = 4.0;
+    private final double jeanY2 = 12.0;
+    private final int LemmyZ = 4;
+    private final double LemmyX1 = 15.0;
+    private final double LemmyY1 = 20.0;
+    private final double LemmyX2 = 25.0;
+    private final double LemmyY2 = 30.0;
+    private final int KerryZ = 6;
+    private final double KerryAE = 28.0;
+    private final double KerryX1 = 15.0;
+    private final double KerryY1 = 25.0;
+    private final double KerryX2 = 40.0;
+    private final double KerryY2 = 20.0;
+    private final double linAEX2 = 12.0;
+    private final double linAEY2 = 12.0;
+    private final double paulX1 = 3.0;
+    private final double paulY1 = 10;
+    private final double paulX2 = 6;
+    private final double paulY2 = 10;
+    private final double thereseX1 = 2;
+    private final double thereseY1 = 6;
+    private final double thereseX2 = 2;
+    private final double thereseY2 = 12.0;
+    private final double pierreX1 = 4;
+    private final double pierreY1 = 5;
+    private final double pierreX2 = 9;
+    private final double pierreY2 = 5;
+    private final double flavieX1 = 4;
+    private final double flavieX2 = 4;
+    private final double flavieY1 = 11.0;
+    private final double flavieY2 = 15.0;
+    private final double claudeX1 = 1;
+    private final double claudeY1 = 4;
+    private final double claudeX2 = 5;
+    private final double claudeY2 = 4;
+    private final double bernardX1 = 4;
+    private final double bernardY1 = 8;
+    private final double bernardX2 = 4;
+    private final double bernardY2 = 10;
+    private final double JacqueX1 = 4;
+    private final double JacqueX2 = 4;
+    private final double JacqueY1 = 16.0;
+    private final double JacqueY2 = 19.0;
+    private final double ddX1 = 3;
+    private final double ddX2 = 1;
+    private final double ddY1 = 14.0;
+    private final double ddY2 = 16.0;
+    private final double circleX = 5;
+    private final double circleY = 4;
+    private final double circleR = 2;
+    private final double circle2X = 4;
+    private final double circle2Y = 9;
+    private final double circle2R = 2;
+    private final double  circle3X = 4;
+    private final double  circle3Y = 17.0;
+    private final double  circle3R = 3;
+    private final double  rectangleX = 4;
+    private final double  rectangleY = 3;
+    private final double  rectangleW = 3;
+    private final double  rectangleH = 4;
+    private final double  rectangle2X = 2;
+    private final double  rectangle2Y = 9;
+    private final double  rectangle2W = 6;
+    private final double  rectangle2H = 3;
+    private final double  squareX = 2;
+    private final double  squareY = 9;
+    private final double  squareL = 6;
+
+
+    private Line lin = new Line("Kim", zorder, x1lin,liny1,linx2,liny2);
+    private Line gaut= new Line("Gaut", zorder, gautx1,gauty1,gautx2,gauty2);
+    private Line marion= new Line("Marion", zorder, marionx1,mariony1,marionx2,mariony2);
+    private Line gaut1= new Line("Gaut1", zorder, gaut1X1,gaut1Y1,gaut1X2,gaut1Y2);
+    private Line marion1= new Line("Marion1", zorder, marion1X1,marion1y1,marion1x2,marion1y2);
+    private Line romain= new Line("Romain", zorder, romainX1,romainY1,romainX2,romainY2);
+    private Line vincent = new Line("Vincent",zorder, vincentX1, vincentY1, vincentX2, vincentY2);
+    private Line jean = new Line("Jean",zorder, jeanX1, jeanY1, jeanX2, jeanY2);
+    private Line paul = new Line("Paul",zorder, paulX1, paulY1, paulX2, paulY2);
+    private Line therese = new Line("Therese",zorder, thereseX1, thereseY1, thereseX2, thereseY2);
+    private Line pierre = new Line("Pierre",zorder, pierreX1, pierreY1, pierreX2, pierreY2);
+    private Line flavie = new Line("Flavie",zorder, flavieX1, flavieY1, flavieX2, flavieY2);
+    private Line claude = new Line("Claude",zorder, claudeX1, claudeY1, claudeX2, claudeY2);
+    private Line bernard = new Line("Bernard",zorder, bernardX1, bernardY1, bernardX2, bernardY2);
+    private Line Jacque = new Line("Jacque",zorder, JacqueX1, JacqueY1, JacqueX2, JacqueY2);
+    private Line dd = new Line("DD",zorder, ddX1, ddY1, ddX2, ddY2);
+    private Circle circle = new Circle("Circle", zorder, circleX, circleY, circleR);
+    private Circle circle2 = new Circle("Circle2", zorder, circle2X, circle2Y, circle2R);
+    private Circle circle3 = new Circle("Circle3", zorder, circle3X, circle3Y, circle3R);
+    private Rectangle rectangle = new Rectangle("Rectangle", zorder,rectangleX, rectangleY, rectangleW, rectangleH);
+    private Rectangle rectangle2 = new Rectangle("Rectangle2",zorder, rectangle2X, rectangle2Y ,rectangle2W,rectangle2H);
+    private Square square = new Square("Square", zorder,squareX, squareY, squareL);
     private Shape[] listShapeGroup = new Shape[]{rectangle2,bernard,paul,circle2,square};
-    private Group group1 = new Group("group1",2,5,listShapeGroup);
+    private final int nbshape2= 2;
+    private final int nbshape5= 5;
+    private Group group1 = new Group("group1",zorder,nbshape5,listShapeGroup);
     private Shape[] listShapeGroup2 = new Shape[]{claude,marion1};
-    private Group group2 = new Group("group2",2,2,listShapeGroup2);
+    private Group group2 = new Group("group2",zorder,nbshape2,listShapeGroup2);
     private Shape[] listShapeGroup3 = new Shape[]{group1,group2};
-    private Group group3 = new Group("group3",2,2,listShapeGroup3);
+    private Group group3 = new Group("group3",zorder,nbshape2,listShapeGroup3);
 
     private final PrintStream standardOut = System.out;
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
@@ -501,10 +160,7 @@ class LineTest {
      */
     @Test
     void getX2() {
-        //x2 cannot be empty or null
-        assertNotNull(lin.getX2());  // make sure r is not null
-        assertNotEquals(" ", qq.getX2()); // make sure r is not empty
-        assertEquals(SaraAE, new Line("Sara",4,SaraX1,SaraY1,SaraX2,SaraY2).getX2());
+        assertEquals(gautx2, gaut.getX2());
 
     }
     /**
@@ -512,10 +168,7 @@ class LineTest {
      */
     @Test
     void getY2() {
-        //Y2 cannot be empty or null
-        assertNotNull(lin.getY2());
-        assertNotEquals(" ", qq.getY2());
-        assertEquals(HenryE, new Line("Henry", 1, HenryX1,HenryY1,HenryX2,HenryY2).getY2());
+        assertEquals(gauty2, gaut.getY2());
     }
 
     /**
@@ -523,8 +176,7 @@ class LineTest {
      */
     @Test
     void leadCoef() {
-        assertEquals(1,new Line("Lem",1,LemX1,LemY1,LemX2,LemY2).leadCoef());
-        assertEquals(1,new Line("Lemmy",4,LemmyX1,LemmyY1,LemmyX2,LemmyY2).leadCoef());
+        assertEquals(1,new Line("Lemmy",LemmyZ,LemmyX1,LemmyY1,LemmyX2,LemmyY2).leadCoef());
 
 
     }
@@ -533,8 +185,7 @@ class LineTest {
      */
     @Test
     void b() {
-        assertEquals(5,new Line("Karen",3,KarenX1,KarenY1,KarenX2,KarenY2).b());
-        assertEquals(KerryAE,new Line("Kerry",4,KerryX1,KerryY1,KerryX2,KerryY2).b());
+        assertEquals(KerryAE,new Line("Kerry",KerryZ,KerryX1,KerryY1,KerryX2,KerryY2).b());
     }
 
     /**
@@ -542,11 +193,14 @@ class LineTest {
      */
     @Test
     void move() {
-        //max_coordinate_x can be negative, positive and 0
-        lin.move(4,5);
-        assertEquals(8,lin.getX());
+        final double dx = 4;
+        final double dy = 5;
+        lin.move(dx,dy);
+        final double xpected = 8;
+        final double xpected2 = 7;
+        assertEquals(xpected,lin.getX());
         assertEquals(linAEX2,lin.getX2());
-        assertEquals(7,lin.getY());
+        assertEquals(xpected2,lin.getY());
         assertEquals(linAEY2,lin.getY2());
 
     }
@@ -564,46 +218,60 @@ class LineTest {
      */
     @Test
     void max_coordinate_x() {
-        //max_coordinate_x can be negative, positive and 0
-        assertEquals(9, new Line("borat",7,boratX1,boratY1,boratX2,boratY2).max_coordinate_x());
-        assertEquals(KhanAE, new Line("Khan",8,KhanX1,5.0f,12.0f,8.0f).max_coordinate_x());
+        final double xpectedmaxx = 5;
+        assertEquals(xpectedmaxx, gaut.max_coordinate_x());
     }
     /**
      * Test getter max coordinate y of the line
      */
     @Test
     void max_coordinate_y() {
-        assertEquals(16, new Line("Peter",8,12.0f,15.0f,17.0f,16.0f).max_coordinate_y());
-        assertEquals(23, new Line("Parker",6,15.0f,23.0f,15.0f,17.0f).max_coordinate_y());
+        final double xpectedmaxy = 10;
+        assertEquals(xpectedmaxy, gaut.max_coordinate_y());
     }
     /**
      * Test getter min coordinate x of the line
      */
     @Test
     void min_coordinate_x() {
-        assertEquals(13, new Line("Dennis",9, 13.0f, 24.0f,17.0f, 19.0f).min_coordinate_x());
-        assertEquals(13, new Line("Dennis",9, 27.0f, 23.0f,13.0f, 12.0f).min_coordinate_x());
+        final double xpectedminx = 2;
+        assertEquals(xpectedminx, gaut.min_coordinate_x());
+
     }
     /**
      * Test getter min coordinate y of the line
      */
     @Test
     void min_coordinate_y() {
-        assertEquals(32, new Line("Jenny",9, 45.0f, 32.0f,54.0f, 73.0f).min_coordinate_y());
-        assertEquals(21, new Line("Dennis",9, 45.0f, 21.0f,42.0f, 32.0f).min_coordinate_y());
+        final double xpectedminy = 7;
+        assertEquals(xpectedminy,gaut.min_coordinate_y());
     }
     /**
      * Test getter min coordinate x of the line
      */
     @Test
     void distancePoint() {
-        assertFalse(lin.distancePoint(3.0f,6.0f));
-        assertTrue(gaut.distancePoint(3.0f,7.96f));
-        assertFalse(gaut.distancePoint(16,2));
-        assertTrue(marion.distancePoint(10.0f,2.0f));
-        assertFalse(marion.distancePoint(33,1));
-        assertTrue(romain.distancePoint(10.0f,6.0f));
-        assertFalse(romain.distancePoint(1,1));
+        final double Point1x = 3;
+        final double Point1y = 6;
+        final double Point2x = 3;
+        final double Point2y = 7.96;
+        final double Point3x = 16;
+        final double Point3y = 2;
+        final double Point4x = 10;
+        final double Point4y = 2;
+        final double Point5x = 33;
+        final double Point5y = 1;
+        final double Point6x = 10;
+        final double Point6y = 6;
+        final double Point7x = 1;
+        final double Point7y = 1;
+        assertFalse(lin.distancePoint(Point1x,Point1y));
+        assertTrue(gaut.distancePoint(Point2x,Point2y));
+        assertFalse(gaut.distancePoint(Point3x,Point3y));
+        assertTrue(marion.distancePoint(Point4x,Point4y));
+        assertFalse(marion.distancePoint(Point5x,Point5y));
+        assertTrue(romain.distancePoint(Point6x,Point6y));
+        assertFalse(romain.distancePoint(Point7x,Point7y));
 
 
     }
